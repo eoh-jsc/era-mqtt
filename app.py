0d6bcbb3351e2c23c9ac8c40b385750b30dbacd7
Son Mac <son.lg@eoh.io>
2023-02-18 22:10:40 +0700

    done create user

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import enum
+import os
+
+from hashlib import sha256
+
 from flask import Flask
+from flask import request
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:password@postgres/emqx'
@@ -8,12 +17,37 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class Permission(enum.Enum):
+    allow = "allow"
+    deny = "deny"
+
+
+class Action(enum.Enum):
+    publish = "publish"
+    subscribe = "subscribe"
+    all = "all"
+
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, default='', nullable=False)
+    salt = db.Column(db.String, default='', nullable=False)
+    is_superuser = db.Column(db.Boolean, default=False, nullable=False)
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return self.username
+
+
+class Acl(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, default='', nullable=False)
+    ipaddress = db.Column(db.String, default='', nullable=False)
+    clientid = db.Column(db.String, default='', nullable=False)
+    topic = db.Column(db.String, nullable=False)
+    permission = db.Column(db.Enum(Permission))
+    action = db.Column(db.Enum(Action))
 
 
 @app.route("/")
@@ -21,19 +55,75 @@ def hello():
     return "Hello, World!"
 
 
-@app.route("/create_user")
-def create_user():
-    user = Users(username='john')
+# @app.route("/create_user")
+# def create_user():
+#     user = Users(username='john')
+#     db.session.add(user)
+#     db.session.commit()
+#     return "User created!"
+#
+#
+# @app.route("/list_user")
+# def list_user():
+#     users = Users.query.all()
+#     print('xxx', users)
+#     return str(users)
+
+
+# TODO os set env API_KEY
+@app.route("/users/", methods=['POST'])
+def user_create():
+    # if request.headers.get('Authorization') != os.getenv('API_KEY'):
+    #     return 'Wrong api key', 403
+
+    username = str(request.form['username'])
+    password = str(request.form['password'])
+
+    username = username.strip()
+    password_hash = sha256(password.encode('utf-8')).hexdigest()
+
+    user = Users(
+        username=username,
+        password_hash=password_hash,
+    )
+
     db.session.add(user)
     db.session.commit()
-    return "User created!"
+    return 'OK', 201
 
 
-@app.route("/list_user")
-def list_user():
-    users = Users.query.all()
-    print('xxx', users)
-    return str(users)
+@app.route("/users/<uuid:key>/", methods=['DELETE'])
+def user_delete(key):
+    # if request.headers.get('Authorization') != os.getenv('API_KEY'):
+    #     return 'Wrong api key', 403
+
+    user = db.get_or_404(Users, username=str(key))
+
+    db.session.delete(user)
+    db.session.commit()
+    return 'OK', 204
+
+
+# TODO make compatiple with type, pattern, read, write
+@app.route("/users/<uuid:key>/acl/", methods=['POST'])
+def acl_create(key):
+    # if request.headers.get('Authorization') != os.getenv('API_KEY'):
+    #     return 'Wrong api key', 403
+
+    username = request.form['username']
+    topic = request.form['topic']
+    permission = request.form['permission']
+    action = request.form['action']
+
+    acl = Acl(
+        username=username,
+        topic=topic,
+        permission=permission,
+        action=action,
+    )
+    db.session.add(acl)
+    db.session.commit()
+    return 'OK', 201
 
 
 if __name__ == "__main__":
