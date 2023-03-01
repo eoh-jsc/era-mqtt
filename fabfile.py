b7c10efea65f82b98532b307e53ae6825d49fba2
Son Mac <son.lg@eoh.io>
2023-03-02 00:08:36 +0700

    cicd

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,21 @@
 from fabric2 import task
 
 
+project_path = "/root/era-mqtt"
+
+
+@task
+def restart_all(c):
+    c.run('docker-compose exec -T app supervisorctl status')
+    c.run('docker-compose exec -T app supervisorctl reread')
+    c.run('docker-compose exec -T app supervisorctl update')
+    c.run('docker-compose exec -T app supervisorctl restart all')
+
+
 @task
 def deploy(c):
-    print("Starting deploy...")
-    with c.cd("/root/era-mqtt"):
-        c.run("git pull")
-        c.run('docker-compose exec -T app supervisorctl status')
-        c.run('docker-compose exec -T app supervisorctl reread')
-        c.run('docker-compose exec -T app supervisorctl update')
-        c.run('docker-compose exec -T app supervisorctl restart uwsgi')
-    print("Done.")
+    print("Starting...")
+    with c.cd(project_path):
+        c.run("git pull")  # TODO
+        restart_all(c)
+    print("Success.")
