83af9f1a7bfa4b4915ab482361007f60b3de195a
Son Mac <son.lg@eoh.io>
2023-03-03 19:59:22 +0700

    missing migrate

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,7 @@ project_path = "/root/era-mqtt"
 
 @task
 def restart_all(c):
+    c.run('docker-compose exec -T app poetry run flask db upgrade')
     c.run('docker-compose exec -T app supervisorctl status')
     c.run('docker-compose exec -T app supervisorctl reread')
     c.run('docker-compose exec -T app supervisorctl update')
