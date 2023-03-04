984f43fc2515b99911fc2fb295161a1713bf4259
Son Mac <son.lg@eoh.io>
2023-03-04 23:20:37 +0700

    poetry install

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,13 @@
 from fabric2 import task
 
 
-project_path = "/root/era-mqtt"
+project_path = '/root/era-mqtt'
 
 
 @task
 def restart_all(c):
     c.run('docker-compose exec -T app poetry run flask db upgrade')
+    c.run('docker-compose exec -T app poetry install')
     c.run('docker-compose exec -T app supervisorctl status')
     c.run('docker-compose exec -T app supervisorctl reread')
     c.run('docker-compose exec -T app supervisorctl update')
@@ -16,8 +17,8 @@ def restart_all(c):
 
 @task
 def deploy(c):
-    print("Starting...")
+    print('Starting...')
     with c.cd(project_path):
-        c.run("git pull")  # TODO
+        c.run('git pull')  # TODO
         restart_all(c)
-    print("Success.")
+    print('Success.')
