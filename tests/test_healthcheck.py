622a81f3db6299796bc9bf4afe03a82be242b4ff
Son Mac <son.lg@eoh.io>
2023-03-15 01:12:53 +0700

    mocker

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -1,34 +1,31 @@
 import pytest
 
-from unittest import mock
 from factories.users import UsersFactory
 
 
-def _test_healthcheck_with_user(client, success):
+def _test_healthcheck_with_user(client, mocker, success):
     UsersFactory()
 
-    with mock.patch('init_app.MqttConnection') as mqtt_connection_class:
-        with mock.patch('init_app.sleep', return_value=None):
-            mock_mqtt_connection = mqtt_connection_class.return_value
-            mock_mqtt_connection.success = success
+    mocker.patch('init_app.sleep', return_value=None)
+    mqtt_connection_class = mocker.patch('init_app.MqttConnection')
+    mock_mqtt_connection = mqtt_connection_class.return_value
+    mock_mqtt_connection.success = success
 
-            response = client.get('/healthcheck')
+    response = client.get('/healthcheck')
 
     return response, mock_mqtt_connection
 
 
-def test_healthcheck_success(client):
-    response, mock_mqtt_connection = _test_healthcheck_with_user(client, True)
-
+def test_healthcheck_success(client, mocker):
+    response, mock_mqtt_connection = _test_healthcheck_with_user(client, mocker, True)
     assert response.status_code == 200
     assert response.data == b'OK'
     mock_mqtt_connection.disconnect.assert_called_once()
 
 
-def test_healthcheck_fail(client):
+def test_healthcheck_fail(client, mocker):
     with pytest.raises(Exception) as excinfo:
-        response, mock_mqtt_connection = _test_healthcheck_with_user(client, False)
-
+        response, mock_mqtt_connection = _test_healthcheck_with_user(client, mocker, False)
         assert 'MQTT connection failed' in str(excinfo.value)
         mock_mqtt_connection.disconnect.assert_not_called()
 
